3b3dbaa1ae57e3c0838c559bd3d1e515d3154ca5
pei223 <ippei.223miyake119@gmail.com>
2018-09-15 16:28:02 +0900

    ブートストラップ、プログレスバー、コピーボタン対応

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import logo from './logo.svg'
+import progressbar from './img/progressbar.png'
 import SearchBox from './SearchBox'
 import './App.css'
 
@@ -29,6 +30,14 @@ export default class App extends React.Component {
     document.getElementById("enText").value = enText
   }
 
+  showProgressBar() {
+    document.getElementById("progressbar-box").style.display = "none"
+    document.getElementById("progressbar-box").style.display = "block"
+  }
+  hideProgressBar() {
+    document.getElementById("progressbar-box").style.display = "none"
+  }
+
   render() {
     // TODO BootStrapでデバイス毎にレスポンシブにする
     return (
@@ -40,15 +49,19 @@ export default class App extends React.Component {
               <h2 className="App-title">メソッド・変数名を日本語から変換します</h2>
             </header>
           </div>
-          <div className="container app-container col-md-offset-3 col-md-6">
-            <SearchBox setEnText={this.setEnText.bind(this)} />
+          <div className="container app-container col-offset-1 col-10 col-sm-offset-1 col-sm-10 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            <SearchBox setEnText={this.setEnText.bind(this)} showProgressBar={this.showProgressBar.bind(this)} hideProgressBar={this.hideProgressBar.bind(this)} />
             <p className="arrow-box">
               <span className="arrow">∨</span>
             </p>
-            
             <div className="row">
-              <input id="enText" type="text" placeholder="英語の関数・変数名" className="form-control" onKeyUp={this.copyEnText}/>
+              <input id="enText" type="text" placeholder="英語の関数・変数名" className="col-9 col-sm-9 col-md-10 col-lg-10 form-control" onKeyUp={this.copyEnText} />
+              <button type="button" className="btn btn-success col-3 col-sm-3 col-md-2 col-lg-2" onClick={this.copyEnText}>Copy</button>
             </div>
+          </div>
+          <div className="progressbar-box" id="progressbar-box">
+            <img src={progressbar} className="progress-circle" id="progressbar" />
+            <p>loading...</p>
           </div>
         </div>
     )
